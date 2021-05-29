150e371cf5a5ce0b4edc6813c7384066da3bd11d
Sahil Kadian <sahilkadian205@gmail.com>
2021-05-29 19:47:50 +0530

    commiting async javascript basics

--- Asynchronous_Javascript/asynchronous_javascript1_basics.js
+++ Asynchronous_Javascript/asynchronous_javascript1_basics.js
@@ -26,7 +26,15 @@ console.log('Statement 3 executed....');
 //1 or 2 seconds.So we don't have to wait for this db call to execute first (waiting for 1 or 2 seconds) and can execute our other code
 //in that time if we use Asynchronous way.
 
-//For implementing Asynchronous way we use following - 
+//Even if we write our code in Asynchronous way we don't have concurrency or in simple words 'multithreading'.We always have single thread.
+//If we write code in Synchronous way then thread will execute code one by one.
+//If we write code in Asynchronous way then thread will execute statement1 then schedule setTimeout() function and execute statement3 and
+//then execute statement after time interval is over defined in setTimeout() method.
+
+//In real world application we store data in a database. So, to deal with database code we should have clear understanding of Asynchronous 
+//way of writing code.
+
+//There are 3 patterns to implement Asynchronous code - 
 //1) Callbacks
 //2) Promises
 //3) Async And Await
